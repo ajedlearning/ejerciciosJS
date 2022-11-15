a53a14842bb5e9f82f50692e691cf3b72f328e72
Arnaldo <arnaldoespinoza1@hotmail.com>
2022-11-15 15:32:50 -0400

    resueltos con validaciones

--- js.js
+++ js.js
@@ -5,31 +5,52 @@ console.log('********************************');
 // **************************jercicio 1*******************************
 console.log('Ejercicio 1');
 function contarCaracteres(cadena = 'Default'){
-    if(cadena.length>1){
+    if(typeof cadena === 'string'){
+
+         if(cadena.length>1){
         return `La cadena ${cadena} tiene ${cadena.length} caracteres`; 
     }
     
-    return `La cadena ${cadena} tiene ${cadena.length} caracter`;
+    return `La cadena ${cadena} tiene ${cadena.length} caracter`;  
+    }else{
+        return console.error('El valor introducido debe ser un String')
+    }
+ 
     
 }
 
-console.log(contarCaracteres('A'))
+console.log(contarCaracteres('5'))
+
+
 
 console.log('********************************');
 console.log('Ejercicio 2');
 
 function recortarTexto(cadena='Default', numeroCaracter=7){
-    
-    return cadena.slice(0, numeroCaracter)
+    if(typeof(cadena)==='string' && !isNaN(numeroCaracter)){
+      return cadena.slice(0, numeroCaracter)  
+    }
+    return console.error('Uno de los tipos de datos no es correcto');
 }
 
-console.log(recortarTexto('Hola como estas', 4));
+console.log(recortarTexto('Hola como estas',3));
 
 console.log('********************************');
 console.log('Ejercicio 3');
 
 function separar(cadena ='Default', separador=''){
-    return cadena.split(separador);
+    if(typeof cadena === 'string' && typeof separador === 'string'){
+        if(cadena.split(separador)[0]===cadena){
+           
+            return console.warn('El sepador incluido no se encuentra en la cadena',cadena);
+        }else{
+            return cadena.split(separador) ; 
+        }
+        
+    }
+
+    return console.error('Uno de los tipos de datos no es correcto');
+   
 }
 console.log(separar('Ayyyryyynyyyayyylyyydyyyo','yyy'));
 
@@ -37,12 +58,16 @@ console.log('********************************');
 console.log('Ejercicio 4');
 
 function repetirN(cadena='Default', veces=10){
-    for (let i = 0; i < veces; i++) {
+
+    if(typeof(cadena)==='string' && !isNaN(veces)){
+        for (let i = 0; i < veces; i++) {
         
-        console.log(cadena + ' ' + (i+1));
+            console.log(cadena + ' ' + (i+1));
+        }
+    }else{
+       return console.error('Debes introducir valores válidos'); 
     }
-    
-    
+
 }
 
-repetirN('hola',10);
+repetirN('hola', 10);

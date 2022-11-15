4c5b53ebd95b2bc5167062a511e0df2959ee62de
Arnaldo <arnaldoespinoza1@hotmail.com>
2022-11-15 16:57:37 -0400

    Otra forma de resolver los ejercicios con arrow function

--- js.js
+++ js.js
@@ -1,73 +1,59 @@
-console.log('Bienvenido');
-console.log('********************************');
-
+console.log('Bienvenido')
+console.log('********************************')
 
 // **************************jercicio 1*******************************
-console.log('Ejercicio 1');
-function contarCaracteres(cadena = 'Default'){
-    if(typeof cadena === 'string'){
+console.log('Ejercicio 1')
 
-         if(cadena.length>1){
-        return `La cadena ${cadena} tiene ${cadena.length} caracteres`; 
+const contarCaracteres = (cadena = '') => {
+  if (typeof cadena === 'string') {
+    if (cadena.length > 1) {
+      console.log(`La cadena ${cadena} tiene ${cadena.length} caracteres`)
+    } else {
+      console.log(`La cadena ${cadena} tiene ${cadena.length} caracter`)
     }
-    
-    return `La cadena ${cadena} tiene ${cadena.length} caracter`;  
-    }else{
-        return console.error('El valor introducido debe ser un String')
-    }
- 
-    
+  } else {
+    console.error(`Dato incorrecto en la cadena`)
+  }
 }
 
-console.log(contarCaracteres('5'))
+contarCaracteres('Arnaldo')
 
+console.log('********************************')
+console.log('Ejercicio 2')
 
-
-console.log('********************************');
-console.log('Ejercicio 2');
-
-function recortarTexto(cadena='Default', numeroCaracter=7){
-    if(typeof(cadena)==='string' && !isNaN(numeroCaracter)){
-      return cadena.slice(0, numeroCaracter)  
-    }
-    return console.error('Uno de los tipos de datos no es correcto');
+const recortarTexto = (cadena = 'Default', numeroCaracter = 7) => {
+  if (typeof cadena === 'string' && !isNaN(numeroCaracter)) {
+    return cadena.slice(0, numeroCaracter)
+  }
+  console.error('Uno de los tipos de datos no es correcto')
 }
 
-console.log(recortarTexto('Hola como estas',3));
+console.log(recortarTexto('Hola como estas', 4))
 
-console.log('********************************');
-console.log('Ejercicio 3');
+console.log('********************************')
+console.log('Ejercicio 3')
 
-function separar(cadena ='Default', separador=''){
-    if(typeof cadena === 'string' && typeof separador === 'string'){
-        if(cadena.split(separador)[0]===cadena){
-           
-            return console.warn('El sepador incluido no se encuentra en la cadena',cadena);
-        }else{
-            return cadena.split(separador) ; 
-        }
-        
+const separar = (cadena = 'Default', separador = '')=>
+  (typeof cadena === 'string' && typeof separador === 'string')
+  ?(cadena.split(separador)[0] === cadena)
+  ?console.warn('El sepador incluido no se encuentra en la cadena ', cadena)
+  :cadena.split(separador)
+  :console.error('Uno de los tipos de datos no es correcto')
+
+
+console.log(separar('Ayyyryyynyyyayyylyyydyyyo', ''))
+
+console.log('********************************')
+console.log('Ejercicio 4')
+
+const repetirN = (cadena = 'Default', veces = 10)=> {
+  if (typeof cadena === 'string' && !isNaN(veces)) {
+    for (let i = 0; i < veces; i++) {
+      console.log(cadena + ' ' + (i + 1))
     }
-
-    return console.error('Uno de los tipos de datos no es correcto');
-   
-}
-console.log(separar('Ayyyryyynyyyayyylyyydyyyo','yyy'));
-
-console.log('********************************');
-console.log('Ejercicio 4');
-
-function repetirN(cadena='Default', veces=10){
-
-    if(typeof(cadena)==='string' && !isNaN(veces)){
-        for (let i = 0; i < veces; i++) {
-        
-            console.log(cadena + ' ' + (i+1));
-        }
-    }else{
-       return console.error('Debes introducir valores válidos'); 
-    }
-
+  } else {
+    return console.error('Debes introducir valores válidos')
+  }
 }
 
-repetirN('hola', 10);
+repetirN('hola', 10)
